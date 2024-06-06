63ce9b13dc36d4671bbbafb9904cb324c4866c20
minuum <minwool0357@gmail.com>
2024-06-06 12:07:54 +0900

    update : pysqlite3-binary

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import Chroma
-from langchain.schema import Document
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -17,46 +16,13 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-
 from chatbot_class import Chatbot
 
 def stream_data(response):
     for word in response.split(" "):
         yield word + " "
         time.sleep(0.02)
-# PDF 파일들을 로드하여 분할한 뒤 텍스트를 벡터로 변환하여 DB에 저장
-def load_and_index_documents(pdf_directory, text_directory, api_key):
-    documents = []
 
-    # PDF 파일들을 로드하여 분할
-    pdf_files = glob(os.path.join(pdf_directory, '*.pdf'))
-    for pdf_file in pdf_files:
-        loader = PyPDFLoader(pdf_file)
-        pdf_documents = loader.load()
-        documents.extend(pdf_documents)
-    print(f"Loaded {len(documents)} documents from {pdf_directory}")
-    # 텍스트 파일들을 로드하여 분할
-    text_files = glob(os.path.join(text_directory, '*.txt'))
-    for text_file in text_files:
-        with open(text_file, 'r', encoding='utf-8') as file:
-            text = file.read()
-            # 텍스트를 Document 객체로 변환
-            text_document = Document(page_content=text, metadata={"source": text_file})
-            documents.append(text_document)
-    print(f"Loaded {len(documents)} documents from {text_directory}")
-    # 분할된 텍스트를 벡터로 변환하여 ChromaDB에 저장
-    chunk_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    chunks = chunk_splitter.split_documents(documents)
-    print(f"Split {len(chunks)} chunks from {len(documents)} documents")
-    print("Embedding documents...",end="")
-    embeddings = OpenAIEmbeddings(api_key=api_key)
-    print("done.")
-    print("Indexing documents...",end="")
-    vectordb = Chroma.from_documents(documents=chunks, embedding=embeddings)
-    print("done.")
-    retriever = vectordb.as_retriever()
-    
-    return retriever
 def pdf_load(dir):
     input_docs = []
     # Load all PDF files using PyPDFLoader
@@ -70,19 +36,11 @@ def pdf_load(dir):
         
 
 documents = []
-
-# PDF와 텍스트 파일을 모두 로드할 디렉토리 경로
-pdf_directory = "./data"
-text_directory = "./crawled_texts"
-
-
-# Load and index documents from both PDF and text files
-
-
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+pdf_directory = './data'
 
 if "OPENAI_API" not in st.session_state:
     st.session_state["OPENAI_API"] = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") else ""
-    # print(st.session_state["OPENAI_API"])
 # 기본 모델을 설정합니다.
 if "model" not in st.session_state:
     st.session_state["model"] = "gpt-4o"
@@ -111,13 +69,31 @@ if "prompt" not in st.session_state:
 #################################################
 
 if "retriever" not in st.session_state:
-    st.session_state.retriever = load_and_index_documents(pdf_directory, text_directory, st.session_state["OPENAI_API"])
+    pdf_files = glob(os.path.join(pdf_directory, '*.pdf'))
+
+    # Load all PDF files using PyPDFLoader
+    for pdf_file in pdf_files:
+        loader = PyPDFLoader(pdf_file)
+        pdf_documents = loader.load()
+        documents.extend(pdf_documents)
+        
+    # 텍스트는 RecursiveCharacterTextSplitter를 사용하여 분할
+    chunk_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = chunk_splitter.split_documents(documents)
+    print("Chunks split Done.")
+    # embeddings은 OpenAI의 임베딩을 사용
+    # vectordb는 chromadb사용함
+
+    embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+    vectordb = Chroma.from_documents(documents=chunks, embedding=embeddings)
+    print("Retriever Done.")
+    st.session_state.retriever = vectordb.as_retriever()
     
 # pdf를 사용해서 pdf(논문)을 모두 로드
 
 if __name__ == '__main__':
     
-    st.title("강남대학교 학사지원 챗봇")
+    st.title("챗-봇")
     # Create a sidebar for API key and model selection
     with st.expander("챗봇 사용법", expanded=False):
         st.markdown("""
